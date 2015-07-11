34d6cf2da3b17458c93daf1c3524ca92be59a3a9
JustinMabbutt <Justin.Mabbutt@netbuilder.com>
2015-07-11 21:35:00 +0100

    More 1.1 implementation

--- src/IMS/StockReportGenerator.java
+++ src/IMS/StockReportGenerator.java
@@ -22,11 +22,53 @@ public class StockReportGenerator
 	
 	private DateFormat dateFormat;
 	private Date date;
-	private String stockReportFilePath;
+	private String stockReportFilePath, purchaseOrderFilePath;
+	BufferedWriter writer;
 	
-	public void createOrderForm(JTable stockTable)
+	public void createPurchaseOrder(JTable stockTable)
 	{
-		
+		logger.entering(getClass().getName(), "createPurchaseOrder");
+		dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		date = new Date();
+		purchaseOrderFilePath = "C:/Users/justi_000/workspace/JustinIMS/PurchaseOrder.txt";
+		try
+		{
+			writer = new BufferedWriter(new FileWriter(purchaseOrderFilePath));
+			writer.write("NB Gardens Purchase Order - " + dateFormat.format(date));
+			writer.newLine();
+			writer.newLine();
+			
+			for(int i = 0; i < stockTable.getColumnCount(); i++)
+			{
+				writer.write(stockTable.getColumnName(i));
+				writer.write("\t");
+			}
+			
+			for(int i = 0; i < stockTable.getRowCount(); i++)
+			{
+				if(Integer.parseInt(String.valueOf(stockTable.getValueAt(i, 2))) < Integer.parseInt(String.valueOf(stockTable.getValueAt(i, 3))))
+				{
+					writer.newLine();
+					for(int j = 0; j < stockTable.getColumnCount(); j++)
+					{
+						writer.write(String.valueOf(stockTable.getValueAt(i, j)));
+						if(String.valueOf(stockTable.getValueAt(i, j)).length() >= 16)
+						{
+							writer.write("\t");
+						}
+						else
+						{
+							writer.write("\t"); writer.write("\t");
+						}
+					}
+				}
+			}
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		logger.exiting(getClass().getName(), "createPurchaseOrder");
 	}
 	
 	public void createStockReport(JTable stockTable)
@@ -38,7 +80,7 @@ public class StockReportGenerator
 		stockReportFilePath = "C:/Users/justi_000/workspace/JustinIMS/StockReport.txt";
 		try
 		{
-			BufferedWriter writer = new BufferedWriter(new FileWriter(stockReportFilePath));
+			writer = new BufferedWriter(new FileWriter(stockReportFilePath));
 			
 			writer.write("NB Gardens Stock Report - " + dateFormat.format(date));
 			writer.newLine();
